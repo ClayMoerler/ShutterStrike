bf93d4cd7bedd8c5b9f91e19bb1376985686dd9f
lucasarabi <lucaarab03@gmail.com>
2025-09-27 00:41:01 -0400

    Initialized JavaSpring project

---
+++ src/main/java/com/example/ShutterStrike/ShutterStrikeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShutterStrike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShutterStrikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShutterStrikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShutterStrike/ShutterStrikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShutterStrike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShutterStrikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
