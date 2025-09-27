3c960cfe4c7fbe0567444d63865aec72d1abaee2
Clay Moerler <74159961+ClayMoerler@users.noreply.github.com>
2025-09-27 04:48:21 -0400

    fixing lombok

--- src/main/java/com/example/ShutterStrike/Models/Player.java
+++ src/main/java/com/example/ShutterStrike/Models/Player.java
@@ -1,8 +1,14 @@
 package com.example.ShutterStrike.Models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Player{
 
+    
     //Player Data
+    @Setter
+    @Getter
     private String playerName; 
     private String playerClass;
     private int playerUUID;
@@ -21,43 +27,7 @@ public class Player{
     //Default Constructor
     public Player(){
         this(null,null,0,0,false);
-    }
 
-    //Setters
-    public void setPlayerName(String playerName){
-        this.playerName = playerName;
-    }
-    public void setPlayerClass(String playerClass){
-        this.playerClass = playerClass;
-    }
-    public void setPlayerID(int playerUUID){
-        this.playerUUID = playerUUID;
-    }
-    public void setPlayerHealth(int playerHealth){
-        this.playerHealth = playerHealth; 
-    }
-    public void setIsHost(boolean isHost){
-        this.isHost = isHost; 
-    }
-
-    //Getters
-    public String getPlayerName(String playerName){
-        return playerName;
-    }
-    public String getPlayerClass(String playerClass){
-        return playerClass;
-    }
-    public int getPlayerID(int playerUUID){
-        return playerUUID;
-    }
-    public int getPlayerHealth(int playerHealth){
-        return playerHealth;
-    }
-    public boolean getIsHost(boolean isHost){
-        return isHost; 
-    }
-
-    public void takeDamage(int damage){
-        this.playerHealth = this.playerHealth - 1;   
     }
 }
+
--- src/main/java/com/example/ShutterStrike/Models/Storm.java
+++ src/main/java/com/example/ShutterStrike/Models/Storm.java
@@ -1,14 +1,35 @@
 package com.example.ShutterStrike.Models;
 
+import java.util.Random;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+import lombok.Getter;
+import lombok.Setter;
+
 public class Storm {
+    @Getter
+    @Setter
     private double centerX;
     private double centerY;
     private double radius;
     private double nextRadius;
     private double shrinkTimer;
 
-    public Storm(double centerX, double centerY, double radius){
+    private final Random random = new Random();
 
+    public Storm(double centerX, double centerY, double radius){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
     }
+
+    @Scheduled(fixedRate = 30000)
+    public void shrinkZone(){
+        
+    }
+    
+
+
 
 }
