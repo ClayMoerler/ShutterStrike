bdd7e83e42cd4c93ba90e18bafd0d46505b9dfc2
Clay Moerler <74159961+ClayMoerler@users.noreply.github.com>
2025-09-27 03:37:11 -0400

    cleaning files

--- src/main/java/com/example/ShutterStrike/Constants/pconstants.java
+++
@@ -1,5 +0,0 @@
-package com.example.ShutterStrike.Constants;
-
-public class pconstants {
-    
-}
--- src/main/java/com/example/ShutterStrike/Models/Storm.java
+++ src/main/java/com/example/ShutterStrike/Models/Storm.java
@@ -1,12 +1,14 @@
 package com.example.ShutterStrike.Models;
 
-public class game {
+public class Storm {
     private double centerX;
     private double centerY;
     private double radius;
     private double nextRadius;
     private double shrinkTimer;
 
-    
+    public Storm(double centerX, double centerY, double radius){
+
+    }
 
 }
