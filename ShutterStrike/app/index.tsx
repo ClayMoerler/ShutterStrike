926627238211d2de5bd67cdd2a33949cc2adb85a
lucasarabi <lucaarab03@gmail.com>
2025-09-27 17:22:39 -0400

    implemented skeleton for user data provision

--- ShutterStrike/app/index.tsx
+++ ShutterStrike/app/index.tsx
@@ -4,6 +4,7 @@ import SettingsIcon from '../assets/images/cog.svg';
 import IconButton from '../components/IconButton';
 import StyledTextInput from '@/components/StyledTextInput';
 import { initLobby } from '@/api/api-calls';
+import { generateUUID } from '@/util/utilities';
 import { useRouter } from 'expo-router'; 
 
 
@@ -28,6 +29,7 @@ export default function Index() {
           onPress={() => {
             router.push('/lobby_host')
             initLobby();
+            generateUUID();
           }}
         />
         
@@ -42,7 +44,10 @@ export default function Index() {
 
           <StyledButton 
             title="Go" 
-            onPress={() => router.push('/lobby_player')}  
+            onPress={() =>{ 
+              router.push('/lobby_player')
+              generateUUID(); 
+            }}
             style={{ width: '30%' }}
           />
 
---
+++ ShutterStrike/util/utilities.ts
@@ -0,0 +1,3 @@
+export const generateUUID = () : String => {
+    return crypto.randomUUID()
+}
