ac9fcfbd422a258dc3f96d4880fbb1d137f13dbb
lucasarabi <lucaarab03@gmail.com>
2025-09-27 19:08:36 -0400

    Added handling for user data provision when no server is avaliable

--- ShutterStrike/api/api-calls.ts
+++ ShutterStrike/api/api-calls.ts
@@ -12,7 +12,10 @@ export const initLobby = async () : Promise<void> => {
     }
 }
 
-export const sendUserData = async() : Promise<void> => {
+// Returns true if server accepts data
+// Returns false if server rejects data
+// Throws error if error encountered (i.e. server not running)
+export const sendUserData = async () : Promise<boolean> => {
 
     const userDataPayload = {
         uuid: generateUUID(),
@@ -20,10 +23,19 @@ export const sendUserData = async() : Promise<void> => {
     }
 
     try {
-        await axios.post(DEV_URI+"/api/player/user", userDataPayload);
-        console.log("Sent user data")
-    }
-    catch(error) {
-        console.log("Failed to send user data")
+        await axios.post<string>(DEV_URI + "/api/player/user", userDataPayload);
+        console.log("Sent user data successfully.");
+        return true;
+
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            if (error.response?.status === 406) {
+                console.log("Server not ready. Rejected with 406.");
+                return false; // The request was rejected as expected
+            }
+        }
+        
+        console.error("An unexpected error occurred:", error);
+        throw error; 
     }
 }
